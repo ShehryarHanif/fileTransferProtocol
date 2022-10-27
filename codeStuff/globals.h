57ca030319aadf0de688ed1edf9c08ce06d2e672
Shehryar Hanif <shehryar.hanif@nyu.edu>
2022-10-27 17:37:33 +0400

    explain global variables

--- codeStuff/globals.h
+++ codeStuff/globals.h
@@ -1,4 +1,8 @@
+// We define the ports as values other than "20" and "21" because they were privileged. Using "sudo" when running the code will allow to use those port numbers
+
 #define CONTROL_PORT 1026
 #define DATA_PORT 1025
+
+// We wanted to divide the content of the data into packets, so as to send information in chunks
 
 #define PACKET_SIZE 47
